0338de263e7a61aed196a8758445d1028e96666f
HomerSilent <145044537+HomerSilent@users.noreply.github.com>
2025-05-30 21:09:03 +0700

    Add files via upload

---
+++ bunny girl senpai folder/localization/index.js
@@ -0,0 +1,16 @@
+var localize = {
+    "en-US": {
+        "home": "Bunny Girl Senpai",
+        "gameCenter": "Game center",
+        "systemApps": "System apps",
+        "installApk": "Install APK",
+        "deleteApps": "Delete apps",
+        "delete": "Delete",
+        "cancel": "Cancel",
+        "areYouSure": "Are you sure you want to delete the selected app. You will loose all the unsaved data.",
+        "installingApp": "Installing app",
+        "newApp": "New app",
+        "installFailed": "Install failed",
+        "retry": "Retry",
+    }
+}
